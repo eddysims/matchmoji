ba1504d238bd22f04742b330a16cb3c0e45d40d3
edeesims <eddysimsweb@gmail.com>
2018-09-20 00:51:20 -0600

    Update the input to radio buttons - needs styling

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -2,13 +2,26 @@ import React from 'react'
 import './Welcome.css'
 
 import Logo from '../Logo/Logo'
+import RadioButton from '../RadioButton/RadioButton'
 
 const Welcome = (props) => {
 	return (
 		<div className="Welcome">
 			<Logo />
 			<form onSubmit={props.submit}>
-				<label>I would like to match <input type="number" name="WelcomeNumber" max="12" min="2" /> emojis</label>
+				How many emojis would you like to match?<br />
+				<RadioButton value="2" />
+				<RadioButton value="3" />
+				<RadioButton value="4" />
+				<RadioButton value="5" />
+				<RadioButton value="6" />
+				<RadioButton value="7" />
+				<RadioButton value="8" />
+				<RadioButton value="9" />
+				<RadioButton value="10" />
+				<RadioButton value="11" />
+				<RadioButton value="12" />
+				<br/>
 				<button>Play Game</button>
 			</form>
 		</div>
